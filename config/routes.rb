a5de137cb2e1414a338f2825de07972f4058ed70
jevazquezb <je.vazquezb@gmail.com>
2022-10-18 11:52:38 -0500

    Setup devise routes for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+  
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'pages#splash_screen'
+
+  #resources :pages, only %i[splash_screen]
+
+  resources :groups, path: 'categories' do
+    resources :expenses, path: 'transactions'
+  end
 end
