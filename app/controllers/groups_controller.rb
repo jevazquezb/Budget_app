8a69091ee42823e4e9a2f15302f741c38b064f5e
jevazquezb <je.vazquezb@gmail.com>
2022-10-19 22:42:13 -0500

    Add groups controllers and views

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,39 @@
+class GroupsController < ApplicationController
+  def index
+    @groups = Group.where(user_id: current_user.id).order(created_at: :desc)
+  end
+
+  def new
+    @group = Group.new
+    @icons = Group.icon_list
+  end
+
+  def create
+    @group = Group.new(group_params)
+    @group.user_id = current_user.id
+
+    respond_to do |format|
+      if @group.save
+        format.html { redirect_to groups_path, notice: 'Category was successfully created.' }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @group = Group.find(params[:id])
+    @group.expenses.each(&:destroy)
+    @group.destroy
+
+    respond_to do |format|
+      format.html { redirect_to groups_path, notice: 'Category was successfully destroyed.' }
+    end
+  end
+
+  def group_params
+    params.require(:group).permit(:name, :icon)
+  end
+
+  private :group_params
+end
