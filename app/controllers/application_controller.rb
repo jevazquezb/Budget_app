9637c3134ccc2ad0181b397120fb452c4e3cdd61
jevazquezb <je.vazquezb@gmail.com>
2022-10-19 22:31:42 -0500

    Add methods to modify paths after sign in and sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,14 @@ class ApplicationController < ActionController::Base
 
   before_action :update_allowed_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(_resource)
+    groups_path
+  end
+
+  def after_sign_up_path_for(_resource)
+    groups_path
+  end
+
   protected
 
   def update_allowed_parameters
